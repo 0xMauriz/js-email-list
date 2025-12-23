e396058f1ca020b09a4ae19ba75550f285466024
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-23 20:31:16 +0100

    Tutti i bonus completati, vorrei aggiungere un altro pò di stile per migliorare l'aspetto della pagina.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
-const outputOne = document.getElementById("listOfMails")
-const endpoint = "https:///flynn.boolean.careers/exercises/api/random/mail"
+const outputOne = document.getElementById("listOfMails");
+const reloadButton = document.getElementById("refetchButton");
+const endpoint = "https:///flynn.boolean.careers/exercises/api/random/mail";
 
 
 for (let i = 0; i < 10; i++) {
@@ -15,3 +16,27 @@ axios.get(endpoint)
         console.error(error.message)
     })
 }
+
+reloadButton.addEventListener ('click', () => {
+
+for (let i = 0; i < 10; i++) {
+axios.get(endpoint)
+    .then(response => {
+
+        let responseOne = response.data;
+
+        if (i === 0) {
+
+        outputOne.innerHTML = ``;
+
+    }
+
+        outputOne.innerHTML += `<li>${responseOne.response}</li>`;
+
+    })
+
+    .catch(error => {
+        console.error(error.message)
+    })
+}
+})
