c727b59569e601619b5517ff397e5038005e6f3d
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-23 17:23:06 +0100

    Esercizio completato con una correzione, inizio Bonus.

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const outputOne = document.getElementById("listOfMails")
+const endpoint = "https:///flynn.boolean.careers/exercises/api/random/mail"
+
+
+for (let i = 0; i < 10; i++) {
+axios.get(endpoint)
+    .then(response => {
+
+        let responseOne = response.data;
+
+        outputOne.innerHTML += `<li>${responseOne.response}</li>`;
+    })
+
+    .catch(error => {
+        console.error(error.message)
+    })
+}
